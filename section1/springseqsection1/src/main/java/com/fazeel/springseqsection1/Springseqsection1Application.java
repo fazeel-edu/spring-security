8e6e924b7b7490ddf941a9890217e474921fc492
Fazeel <mffazeelmohamed2@gmail.com>
2025-03-26 22:39:20 +0400

    commit section1

---
+++ section1/springseqsection1/src/main/java/com/fazeel/springseqsection1/Springseqsection1Application.java
@@ -0,0 +1,13 @@
+package com.fazeel.springseqsection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springseqsection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springseqsection1Application.class, args);
+	}
+
+}
